00ba77ef78b7f808862e59c0af81ec39e69b1c98
mfusillo <matt.fusillo@gmail.com>
2020-05-07 17:51:31 +0100

    initial commit

---
+++ src/Selector.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Selector = () => {
+  return (
+    <h1>Starting Point</h1>
+  )
+}
+
+export default Selector;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Selector from './Selector';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Selector />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
