f840e73ae6a1958e3eebc09f483f3ca617b49c75
mfusillo <matt.fusillo@gmail.com>
2020-05-09 19:18:37 +0100

    Updates

---
+++ precache-manifest.af807cafeae2adff85b0901a1c6fa8d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50e7b348d21ab35a3c7d9d3118d6a7ae",
+    "url": "/coding-exercise/index.html"
+  },
+  {
+    "revision": "5570beb958606d563022",
+    "url": "/coding-exercise/static/css/main.e41ab726.chunk.css"
+  },
+  {
+    "revision": "034a5fbfeffc740b6c41",
+    "url": "/coding-exercise/static/js/2.90312f75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coding-exercise/static/js/2.90312f75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5570beb958606d563022",
+    "url": "/coding-exercise/static/js/main.174158a9.chunk.js"
+  },
+  {
+    "revision": "95bd94cb3da448f0158b",
+    "url": "/coding-exercise/static/js/runtime-main.08e0c814.js"
+  }
+]);
